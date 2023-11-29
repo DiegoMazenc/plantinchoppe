2ad11e06652a12030db6d146f32e08da3babbaca
Diégo Mazenc <contact@diego-mazenc.com>
2023-11-29 17:12:34 +0100

    mise en place du aside

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import NavBar from './components/NavBar.vue'
+import Aside from './components/Aside.vue'
 
 </script>
 
 <template>
  <NavBar/>
+ <main>
+  <Aside/>
+ </main>
+ 
 </template>
 
 <style scoped>
+main{
+  display: flex;
+}
+
 
 </style>
--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,3 +1,26 @@
 <script>
-
+export default{
+    name: "Aside"
+}
 </script>
+
+<template>
+    <div class="aside">
+        <h3>Trier par</h3>
+        <div>Alphabétique</div>
+        <div>Prix</div>
+        <hr/>
+        <div>type</div>
+        <div>difficulté</div>
+        <div>couleur</div>
+    </div>
+</template>
+
+<style>
+.aside{
+    position: relative;
+    background-color: rgb(62, 135, 122);
+height: 100vh;
+width: 30%;
+}
+</style>
