e90f9e093a148c803936e10a4912884d620b54a5
Diégo Mazenc <contact@diego-mazenc.com>
2023-11-29 16:23:42 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <HelloWorld/>
+  <p>coucou</p>
+</template>
+
+<style scoped>
+body{
+  background-color: rgb(36, 36, 36);
+}
+</style>
