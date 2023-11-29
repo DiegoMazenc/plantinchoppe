992c95bd1075f608b9d0ed1a5e7401f2a8c80cab
Lucas Berthet <lu.berthet@gmail.com>
2023-11-29 17:36:48 +0100

    add and style footer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import NavBar from './components/NavBar.vue'
-
+import NavBar from './components/NavBar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
  <NavBar/>
+
+ <Footer class="footer-container"/>
+
 </template>
 
 <style scoped>
 body{
   background-color: rgb(36, 36, 36);
 }
+
+.footer-container{
+    background-color: green;
+}
+
 </style>
