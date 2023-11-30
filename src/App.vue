9b31ad548ec414000371c6c03a0a4df1ee059113
Lucas Berthet <lu.berthet@gmail.com>
2023-11-30 10:00:06 +0100

    add cards logic creation

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <script setup>
-import NavBar from './components/NavBar.vue';
-import Footer from './components/Footer.vue';
-import PlantsCard from './components/PlantsCard.vue';
+
 </script>
 
 <template>
- <NavBar/>
- <PlantsCard/>
-
- <Footer class="footer-container"/>
 
 </template>
 
 <style scoped>
-body{
-  background-color: rgb(36, 36, 36);
-}
 
-.footer-container{
-    background-color: green;
-}
 
 </style>
--- src/components/PlantsCard.vue
+++ src/components/PlantsCard.vue
@@ -1,36 +1,34 @@
 <script>
 
-import plants from '../data/plants.js'
+import plants from '../data/plants.json'
 
 export default {
   name: 'PlantsCard',
 
   //The imported module should be store as an object so a new proprety as to be set
   data() {
-    return plants; 
-  }, 
-  methods: {
-    afficher() {
-        console.log(this.plants); 
-    }
+    return {plantesListe: plants}; 
+  },
+  mounted() {
+    console.log(this.plantesListe); 
   },
 }
 
 </script>
 
 <template>
-  <div class="card" style="width: 18rem">
+
+  <div v-for="(plante, index) in plantesListe" :key="index" class="card" style="width: 18rem">
     <img src="..." class="card-img-top" alt="..." />
     <div class="card-body">
-      <h5 class="card-title">Yo</h5>
+      <h5 class="card-title">{{ plante.name }}</h5>
       <p class="card-text">
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
+      {{plante.price}}
       </p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
-      <a href="#" class="btn btn-primary" @click="afficher">Afficher dans la console</a>
     </div>
   </div>
+
 </template>
 
 <style scoped></style>
